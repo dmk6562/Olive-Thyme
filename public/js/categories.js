39a03612d2376327db84520e112f5fa60cc3edce
Mariam Chaudry <mariamschaudry1@gmail.com>
2018-06-03 00:53:33 -0400

    fixed some styling

--- public/js/categories.js
+++ public/js/categories.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // Getting references to the name input and author container, as well as the table body
-    var nameInput = $("#author-name");
+    var nameInput = $("#category-name");
     var authorList = $("tbody");
     var authorContainer = $(".author-container");
     // Adding event listeners to the form to create a new object, and the button to delete
